58d3de6c39ea49c451ef890a6d928a21ed24399b
IsabellaBacalao <isabella.bacalao@decathlon.com>
2024-02-15 16:31:56 +0100

    feat: implement github actions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const [postId, setPostId]= useState();
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "model": "mistral", // installed model, mandatory
+      "prompt": "Why is the sky blue?", // your prompt
+      "stream": false // wait for the response to be complete before answering. You can use true, and implement streamed responses.
+       })
+    };
+    fetch('http://gprrrr.co/ollama/api/generate', requestOptions)
+        .then(response => response.json())
+        .then(data => setPostId(data.id))
+        .then(console.log(postId));
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename='/MichelleAndIsabella'>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
